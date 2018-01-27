1ff25f9dccbb2dba85e42f040f4dd171dec92a70
Wei Zhang <ink.wj.zhang@gmail.com>
2018-01-26 17:20:48 -0800

    Basic CI to Good CI conversion

---
+++ application/views/_cell.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wei
+ * Date: 1/26/2018
+ * Time: 4:39 PM
+ */
+?>
+<a href="./data/{filename}" data-lightbox="gallery"
+   data-title="{title}... Posted {uploaded} by {uploader}, in {category}">
+    <img src="./data/thumb/{filename}"/>
+</a>
